d9ec4bc3a96f67e55d6d6d1947286c063d5a08f0
Anderson <andersonadauto2016@gmail.com>
2025-07-04 18:07:15 -0300

    implementando parte visual do menu implementando constantes

--- code/game.py
+++ code/game.py
@@ -2,20 +2,17 @@
 # -*- coding: utf-8 -*-
 import pygame
 from code.Menu import Menu
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 
 class Game:
     def __init__(self):
         pygame.init()
-        LARGURA_DA_TELA = 600
-        ALTURA_DA_TELA = 480
-        self.window = pygame.display.set_mode(size=(LARGURA_DA_TELA, ALTURA_DA_TELA))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
+     
+
         while True:
             menu = Menu(self.window)
             menu.run()
-            # Check for all events
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit() # Close Window
-            #         quit() #end py game
+
