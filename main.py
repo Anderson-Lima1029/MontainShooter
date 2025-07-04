c945ba5eb24ec910486cd2339fbbf295f8821248
Anderson <andersonadauto2016@gmail.com>
2025-07-04 16:01:06 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+print("Setup Start")
+pygame.init()
+LARGURA_DA_TELA = 600
+ALTURA_DA_TELA = 480
+screen = pygame.display.set_mode(size=(LARGURA_DA_TELA, ALTURA_DA_TELA))
+print("Setup End")
+
+print("Loop Start")
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Close Window
+            quit() #end py game
